499cc4c637090db80a53d9c7f916d34fb352576e
Richard Findlay <rfindlay1992@gmail.com>
2021-10-21 23:51:23 +0100

    model and updated sequencing method implemented

--- solarScriptVersion_testMethod.py
+++ solarScriptVersion_testMethod.py
@@ -27,7 +27,9 @@ import seaborn as sns
 import random
 
 ###########################################_____LOAD_PROCESSED_DATA_____############################################
-dataset_name = 'train_set_V11_withtimefeatures_120hrinput.hdf5'
+dataset_name = 'train_set_V5_withtimefeatures_120hrinput_float32.hdf5'
+model_type = 'wind'
+
 
 # load training data dictionary
 # train_set_load = open("/content/drive/My Drive/Processed_Data/train_set_V6_withtimefeatures_120hrinput_.pkl", "rb") 
@@ -48,12 +50,10 @@ x_len = f['train_set']['X1_train'].shape[0]
 y_len = f['train_set']['y_train'].shape[0]
 print('size parameters loaded')
 
-# print(x_len)
-# print(y_len)
-# sys.exit()
+input_seq_size = 240
+output_seq_size = 48
 
 f.close()
-
 
 ###########################################_____DATA_GENERATOR_____#################################################
 
@@ -68,158 +68,74 @@ class DataGenerator(tensorflow.keras.utils.Sequence):
 		self.shuffle = shuffle
 		self.xlen = x_length
 		self.ylen = y_length 
-		self.input_idx = 0
 		self.on_epoch_end()
-
-		self.idx = 0 
-		self.idx_count = self.batch_size / 48
-		self.previous_idx = 0  
-		self.prev_y_true = None    
 
 	def __len__(self):
 		# 'number of batches per Epoch'
-		return int(np.floor(self.ylen / self.batch_size))
+		return int(np.floor(self.xlen / (self.batch_size + (input_seq_size-1))))
 
 	def __getitem__(self, index):
 
-		# # input batch ref updates 
-		# indexes = []
-
-		# while len(indexes) < self.batch_size: 
-
-		# 	if self.previous_idx != 0:
-		# 		idxs = [self.idx-1 for x in range(self.previous_idx)]
-		# 		indexes.extend(idxs)
-		# 		self.idx_count -=  (self.previous_idx / 48)
-		# 		self.previous_idx = 0 
-
-		# 	if self.idx_count >= 1:
-		# 		for i in range(int(np.floor(self.idx_count))):
-		# 			idxs = [self.idx for x in range(48)]
-		# 			indexes.extend(idxs)
-		# 			self.idx += 1 
-		# 			self.idx_count -= 1
-
-		# 	if self.idx_count < 1:
-		# 		idx_len = self.idx_count * 48
-		# 		idxs = [self.idx for x in range(int(idx_len))]
-		# 		indexes.extend(idxs)
-		# 		self.idx_count -= (int(idx_len) / 48)
-
-		# 		# how much of the prev index we need to include in the next batch
-		# 		if 48 - int(idx_len) < self.batch_size:
-		# 			self.previous_idx = 48 - int(idx_len)
-		# 		else:
-		# 			self.previous_idx = 0
-
-		# 	if np.round(abs(self.idx_count),4) == 0:            
-		# 		self.idx += 1
-		# 		self.idx_count = self.batch_size / 48 
-
-		# print(indexes)
-		# asset index len matches batch size
-		# assert len(indexes) == self.batch_size, "batch size and constructed indexes length do not match"
-
-		input_indexes = self.input_indexes[index * self.batch_size:(index+1) * self.batch_size]
-		output_indexes = self.output_indexes[index * self.batch_size:(index+1) * self.batch_size]
+		input_indexes = self.input_indexes[index*(self.batch_size + (input_seq_size-1)) :(index+1)*(self.batch_size + (input_seq_size-1))]
+		output_indexes = self.output_indexes[index*(self.batch_size + (input_seq_size-1)):(index+1)*(self.batch_size + (input_seq_size-1))]
 
 		# Generate data
-		(X_train1, X_train2, X_train3, decoder_input), y_train = self.__data_generation(input_indexes, output_indexes)
+		(X_train1, X_train2, X_train3, s0, c0), y_train = self.__data_generation(input_indexes, output_indexes)
 
-		return (X_train1, X_train2, X_train3, decoder_input), (y_train, [], []) # pass empty training outputs to extract extract attentions
+		return (X_train1, X_train2, X_train3, s0, c0), (y_train, [], []) # pass empty training outputs to extract extract attentions
 
 	def on_epoch_end(self):
 		# set length of indexes for each epoch
-		self.input_indexes = []
+		self.input_indexes = np.arange(self.xlen)
 		self.output_indexes = np.arange(self.ylen)
-
-		# # input batch ref updates 
-		# indexes = []
-		# previous_idx = 0
-		# idx = 0
-		# batch_size = 24
-		# idx_count = batch_size / 48
-
-		# while len(indexes) < self.ylen: 
-
-		# 	if previous_idx != 0:
-		# 		idxs = [idx-1 for x in range(previous_idx)]
-		# 		indexes.extend(idxs)
-		# 		idx_count -=  (previous_idx / 48)
-		# 		previous_idx = 0 
-
-		# 	if idx_count >= 1:
-		# 		for i in range(int(np.floor(idx_count))):
-		# 			idxs = [idx for x in range(48)]
-		# 			indexes.extend(idxs)
-		# 			idx += 1 
-		# 			idx_count -= 1
-
-		# 	if idx_count < 1:
-		# 		idx_len = idx_count * 48
-		# 		idxs = [idx for x in range(int(idx_len))]
-		# 		indexes.extend(idxs)
-		# 		idx_count -= (int(idx_len) / 48)
-
-		# 		# how much of the prev index we need to include in the next batch
-		# 		if 48 - int(idx_len) < batch_size:
-		# 			previous_idx = 48 - int(idx_len)
-		# 		else:
-		# 			previous_idx = 0
-
-		# 	if np.round(abs(idx_count),4) == 0:            
-		# 		idx += 1
-		# 		idx_count = batch_size / 48 
-
-
-		xlist_idxs = np.arange(self.xlen)
-		for idx in xlist_idxs:
-			repeat_idx = [idx] * 48 
-			self.input_indexes.extend(repeat_idx)     
-
-
+ 
 		if self.shuffle == True:
 			np.random.shuffle(self.input_indexes)
 
+	def to_sequence(self, x1, x2, x3, y):
+		# convert timeseries batch in sequences
+		input_start, output_start = 0, 0
+
+		seqX1, seqX2, seqX3, seqY = [], [], [], []
+
+		while (input_start + input_seq_size) <= len(x1):
+			# offset handled during pre-processing
+			input_end = input_start + input_seq_size
+			output_end = output_start + output_seq_size
+
+			# inputs
+			seqX1.append(x1[input_start:input_end])
+			seqX2.append(x2[input_start:input_end])
+
+			# outputs
+			seqX3.append(x3[output_start:output_end])
+			seqY.append(y[output_start:output_end])
+
+			input_start += 1  
+			output_start += 1
+            
+		seqX1, seqX2, seqX3, seqY = np.array(seqX1), np.array(seqX2), np.array(seqX3), np.array(seqY)
+
+		return seqX1, seqX2, seqX3, seqY
+
 	def __data_generation(self, input_indexes, output_indexes):
 
-		# X_train1 = np.empty([features.shape[0], features.shape[1], features.shape[2], features.shape[3]])
-		# X_train2 = np.empty((times_in.shape[1], times_in.shape[2]))
-
-		X_train1 = []
-		X_train2 = []
-
-		f = h5py.File(f"/content/drive/My Drive/Processed_Data/{self.dataset_name}", "r")    
-		for i in input_indexes:        
-			X_train1_read = f['train_set']['X1_train'][i]
-			X_train2_read = f['train_set']['X2_train'][i]
-
-			X_train1.append(X_train1_read) 
-			X_train2.append(X_train2_read)
-
-		X_train1 = np.stack(X_train1, axis=0)
-		X_train2 = np.stack(X_train2, axis=0)       
-
+		f = h5py.File(f"/content/drive/My Drive/Processed_Data/{self.dataset_name}", "r")      
+		X_train1 = f['train_set']['X1_train'][input_indexes]
+		X_train2 = f['train_set']['X2_train'][input_indexes]
 		X_train3 = f['train_set']['X3_train'][output_indexes]
+
 		y_train = f['train_set']['y_train'][output_indexes]
-
-		X_train3 = np.expand_dims(X_train3, axis=1)
-		y_train = np.expand_dims(y_train, axis=1)
- 
-
-		if self.prev_y_true is not None:
-			decoder_input = self.prev_y_true
-		else:
-			intial_input = np.average(X_train1, axis=(2,3))
-			decoder_input = intial_input[:, -48, 0]
-			decoder_input = np.expand_dims(decoder_input, axis=1)
-			decoder_input = np.expand_dims(decoder_input, axis=-1)
-
-		self.prev_y_true = decoder_input
-
+		# decoder_input = f['train_set']['y_train'][output_indexes]
 		f.close()  
+		
+        # convert to sequence data
+		X_train1, X_train2, X_train3, y_train = self.to_sequence(X_train1, X_train2, X_train3, y_train)
 
-		return (X_train1, X_train2, X_train3, decoder_input), y_train
+		s0 = np.zeros((self.batch_size, n_s))
+		c0 = np.zeros((self.batch_size, n_s))
+     
+		return (X_train1, X_train2, X_train3, s0, c0), y_train
 
 training_generator = DataGenerator(dataset_name = dataset_name, x_length = x_len, y_length = y_len,  **params)
 
@@ -227,9 +143,9 @@ training_generator = DataGenerator(dataset_name = dataset_name, x_length = x_len
 ###########################################_____MODEL_ARCHITECTURE_____#################################################
 
 # cpature some usful dimensions
-Tx = features.shape[0]
-Ty = labels.shape[0]
-height, width, channels = features.shape[1], features.shape[2], features.shape[3]
+Tx = input_seq_size
+Ty = output_seq_size
+height, width, channels = features.shape[0], features.shape[1], features.shape[2]
 times_in_dim = times_in.shape[-1]
 times_out_dim = times_out.shape[-1]
 n_s = 128
@@ -268,7 +184,8 @@ class attention(tf.keras.layers.Layer):
 
 		context = attn_w * enc_output
 		context = tf.reduce_sum(context, axis=1)
-		context = K.expand_dims(context, axis=1)
+		# context = K.expand_dims(context, axis=1)
+		context = RepeatVector(Ty)(context)
 
 		return [attn_w, context]
 
@@ -286,8 +203,6 @@ temporal_attn = attention(n_s, name="temporal_attention")
 spatial_attn = attention(n_s, name="spatial_attention")
 
 
-
-
 def cnn_encoder(ccn_input):
     # input shape -> (batch, time, width, height, features)
     # output shape -> (batch, time, width x height, embedding_size)
@@ -299,14 +214,15 @@ def cnn_encoder(ccn_input):
 	ccn_enc_output = TimeDistributed(Conv2D(64, kernel_size=3, strides=1, activation="relu"))(ccn_enc_output)
 	# ccn_enc_output = BatchNormalization()(ccn_enc_output)  
 	ccn_enc_output = TimeDistributed(Conv2D(128, kernel_size=3, strides=1, activation="relu"))(ccn_enc_output)
-	print(ccn_enc_output.shape)
 
 	ccn_enc_output = Reshape((ccn_enc_output.shape[1], -1, ccn_enc_output.shape[-1]))(ccn_enc_output) 
-	# print(ccn_enc_output.shape)
 
 	ccn_enc_output = K.mean(ccn_enc_output, axis=1) 
     
 	return ccn_enc_output
+
+# encoder layers
+lstm_encoder = LSTM(n_s, return_sequences = True, return_state = True)
 
 
 def encoder(input, times_in):
@@ -320,67 +236,122 @@ def encoder(input, times_in):
 
 	return enc_output, enc_h, enc_s
 
+lstm_decoder = LSTM(n_s, return_sequences = True, return_state = True)
 
+def decoder(context, h_state, cell_state):
 
-# encoder / decoder layers
-encoder_lstm = LSTM(n_s, return_sequences=True, return_state = True)
-decoder_lstm = LSTM(n_s, return_sequences=True ,return_state = True)
+    # concat encoder input and time features
+	# context = concatenate([context, times_out], axis=-1)
+    
+	dec_output, h_state , c_state = state = lstm_decoder(context, initial_state = [h_state, cell_state])
+# decoder = LSTM(n_s, return_sequences = True, return_state = True)
 
+	return dec_output, h_state, c_state
 
 # layer for output predictions
 predict_1 = Conv1D(64, kernel_size=1, strides=1, padding="same", activation="relu")
 predict_2 = Conv1D(16, kernel_size=1, strides=1, padding="same", activation="relu")
 predict_3 = Conv1D(1, kernel_size=1, strides=1, padding="same", activation="linear")
 
+# dropout
+drop = Dropout(0.3)
+
+# predict_1 = Dense(32, activation="linear")
+# predict_2 = Dense(1, activation="linear")
+
+# full model
+
 
 # define inputs
 x_input = Input(shape=(Tx, height, width, channels))
 times_in = Input(shape=(Tx, times_in_dim))
-times_out = Input(shape=(1, times_out_dim))
-y_prev = Input(shape=(1, 1))
+times_out = Input(shape=(Ty, times_out_dim))
+s_state0 = Input(shape=(n_s,))
+c_state0 = Input(shape=(n_s,))
+dec_inp = Input(shape=(None, 1))
 
+s_state = s_state0
+c_state = c_state0
+
+# create empty list for outputs
+all_predictions = []
+all_temp_attn_weights = []
+all_spat_attn_weights = []
 
 # call CCN_encoder function
 ccn_enc_output = cnn_encoder(x_input)
 
-# reduce x for lstm input
-enc_spatial_inp = K.mean(x_input, axis=(2,3))
-
-# concat input time features with lstm encoder input
-enc_spatial_inp = concatenate([enc_spatial_inp, times_in], axis=-1)
-
 # call LSTM_encoder function
-lstm_enc_output, state_h, state_c = encoder_lstm(enc_spatial_inp)
+lstm_enc_output, enc_s_state, enc_c_state = encoder(x_input, times_in)
 
-# save encoder states 
-encoder_states = [state_h, state_c]
+s_state = enc_s_state
+c_state = enc_c_state
+
+# y_prev = K.mean(x_input, axis=(2,3))
+# y_prev = y_prev[:, -49, 0:1]
+# define intial decoder input
+# y_prev = lstm_enc_output[:,-1,-1]
+# y_prev = K.expand_dims(y_prev, axis=1)
+# y_prev = K.expand_dims(y_prev, axis=-1)
+# y_prev = K.expand_dims(y_prev, axis=-1)
+
+# for t in range(Ty):
 
 # get context matrix (temporal)
-attn_weights_temp, context_temp = temporal_attn(lstm_enc_output, state_h, state_c)
+attn_weights_temp, context_temp = temporal_attn(lstm_enc_output, s_state, c_state)
 
 # get context matrix (spatial)
-attn_weights_spat, context_spat = spatial_attn(ccn_enc_output, state_h, state_c)
+attn_weights_spat, context_spat = spatial_attn(ccn_enc_output, s_state, c_state)
 
 # combine spatial and temporal context
 context = concatenate([context_spat, context_temp], axis=-1) 
 
-# add known output times
-# context = tf.keras.backend.squeeze(context, axis=1)
-# context = RepeatVector(Ty)(context)
-decoder_input = concatenate([context, times_out], axis=-1)
+# print(context.shape)
+
+decoder_input = concatenate([context_spat, times_out], axis=-1) 
 
 # get the previous value for teacher forcing
-decoder_input = concatenate([decoder_input, y_prev], axis=-1)
+# decoder_input = concatenate([context, y_prev], axis=-1)
+
+# decoder_input = drop(decoder_input)
+
+# output times for input to decoder
+# times_out_single = times_out[:,t,:]
+# times_out_single = K.expand_dims(times_out_single , axis=1)
+
+# decoder_input = Dropout(0.2)(decoder_input)
+# decoder_input = concatenate([decoder_input, times_out_single], axis=-1)
 
 # call decoder
-dec_output, _, _ = decoder_lstm(decoder_input, initial_state=encoder_states)
+dec_output, s_state, c_state = decoder(decoder_input, s_state, c_state)
 
 # get final predicted value
 output = predict_1(dec_output)	
 output = predict_2(output)
-output = predict_3(output)
+predictions = predict_3(output)
+
+	# output = Conv1D(64, kernel_size=1, strides=1, padding="same", activation="relu", name=f"1conv_{t}")(dec_output)
+	# output = Conv1D(16, kernel_size=1, strides=1, padding="same", activation="relu", name=f"2conv_{t}")(output)
+	# output = Conv1D(1, kernel_size=1, strides=1, padding="same", activation="linear", name=f"3conv_{t}")(output)
+
+	# if t == 0:
+	# 	predictions = output
+	# 	attentions_temp = attn_weights_temp
+	# 	attentions_spat = attn_weights_spat
+	# else:
+	# 	predictions = concatenate([predictions, output], axis=1, name='predictions_{}'.format(t))
+	# 	attentions_temp = concatenate([attentions_temp, attn_weights_temp], axis=-1, name='attentions_temp_{}'.format(t))
+	# 	attentions_spat = concatenate([attentions_spat, attn_weights_spat], axis=-1, name='attentions_spat_{}'.format(t))
 
 
+	# get previous actual value for teacher forcing
+	# if random.random() < 0.5:
+	# 	print('real')       
+	# y_prev = dec_inp[:, t, :]
+	# y_prev = K.expand_dims(y_prev , axis=1) 
+	# else:
+	# 	print('prediction') 
+	# y_prev = output
 	# y_prev = Reshape((1, 1))(y_prev) 
 	# y_prev = output
 	# y_prev = drop(y_prev)   
@@ -406,7 +377,7 @@ output = predict_3(output)
 # attention_temporal = Lambda(lambda x: concatenate(x, axis=1))(all_temp_attn_weights)
 # attention_spatial = Lambda(lambda x: concatenate(x, axis=1))(all_spat_attn_weights)
 
-model = Model(inputs = [x_input, times_in, times_out, y_prev], outputs = [output, attn_weights_temp, attn_weights_spat])
+model = Model(inputs = [x_input, times_in, times_out, s_state0, c_state0], outputs = [predictions, attn_weights_temp, attn_weights_spat])
 
 
 # define the pinball loss function to optimise
@@ -420,7 +391,7 @@ quantiles = ['0.' + str(i) for i in range(1,10)]
 quantiles = list(map(float, quantiles))
 quantiles.append(0.99)
 quantiles.insert(0, 0.01)
-quantiles = [0.5, 0.9]
+quantiles = [0.5, 0.1, 0.9]
 print(quantiles)
 
 #include clipvalue in optmisier
@@ -480,16 +451,7 @@ def freeze_decoder_train(model):
 
 # 	model.save(f'/content/drive/My Drive/quantile_{q}/solarGeneration_forecast_MainModel_Q_{q}.h5')	
 # 	K.clear_session()
-
-
-# dec_state_input_h = Input(shape=(n_s,))
-# dec_state_input_c = Input(shape=(n_s,))
-# dec_states_inputs = [dec_state_input_h, dec_state_input_c]
-# dec_output, state_h, state_c = decoder_lstm(decoder_input, initial_state=dec_states_inputs)
-# decoder_states = [state_h, state_c]
-# decoder_model = Model([decoder_input] + dec_states_inputs, [output] + decoder_states)
-
-
+		
 
 
 
@@ -499,20 +461,18 @@ for q in quantiles:
 	# model = solarGenation_Model()
 	model.compile(loss = [lambda y,f: defined_loss(q,y,f), None, None], optimizer= optimizer, metrics = ['mae'])
 	print(model.summary())
-	train = model.fit(training_generator, workers=1, epochs = 1, shuffle=True)
+	train = model.fit(training_generator, workers=4, epochs = 30)
 
-	os.mkdir(f'/content/drive/My Drive/solarModels/q_{q}')
+	os.mkdir(f'/content/drive/My Drive/{model_type}Models/q_{q}')
 	# model_freeze.save_weights('/content/drive/My Drive/solarGeneration_forecast_weights_freezed' + '_Q_%s' %(q) + '.h5')
-	model.save(f'/content/drive/My Drive/solarModels/q_{q}/solarGeneration_forecast_MainModel' + '_Q_%s' %(q) + '.h5')
+	model.save(f'/content/drive/My Drive/{model_type}Models/q_{q}/{model_type}Generation_forecast_MainModel' + '_Q_%s' %(q) + '.h5')
 	# model.save_weights('/content/drive/My Drive/solarGeneration_forecast_weights_test2' + '_Q_%s' %(q) + '.h5')
 	
 	# save some additional models for inference
-	enoder_temporal_model = Model(inputs = [x_input, times_in], outputs=[lstm_enc_output, encoder_states])
+	enoder_temporal_model = Model(inputs = [x_input, times_in], outputs=[lstm_enc_output, enc_s_state, enc_c_state])
 	enoder_spatial_model = Model(x_input, ccn_enc_output)
-
-	enoder_temporal_model.save(f'/content/drive/My Drive/solarModels/q_{q}/solarGeneration_encoderModelTemporal' + '_Q_%s' %(q) + '.h5')
-	enoder_spatial_model.save(f'/content/drive/My Drive/solarModels/q_{q}/solarGeneration_encoderModelSpatial' + '_Q_%s' %(q) + '.h5')
-	model.save(f'/content/drive/My Drive/solarModels/q_{q}/solarGeneration_main' + '_Q_%s' %(q) + '.h5')
+	enoder_temporal_model.save(f'/content/drive/My Drive/{model_type}Models/q_{q}/{model_type}Generation_encoderModelTemporal' + '_Q_%s' %(q) + '.h5')
+	enoder_spatial_model.save(f'/content/drive/My Drive/{model_type}Models/q_{q}/{model_type}Generation_encoderModelSpatial' + '_Q_%s' %(q) + '.h5')
 
 	K.clear_session()
 
