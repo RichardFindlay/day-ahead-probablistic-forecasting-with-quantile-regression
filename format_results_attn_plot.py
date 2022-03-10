d805058923fa60ff417ee726691cbd1152b701ef
Richard Findlay <rfindlay1992@gmail.com>
2022-03-10 23:54:29 +0000

    script to convert results into the required d3 plot format

---
+++ format_results_attn_plot.py
@@ -0,0 +1,65 @@
+# format attention results for context plot
+import numpy as np
+import pandas as pd
+from datetime import datetime, timedelta
+import csv
+
+# input params
+input_sequence_len = 336 
+input_num_of_days = input_sequence_len / 48
+start_date = datetime.strptime("01-01-2013", "%d-%m-%Y")
+target_data =  datetime.strptime("08-01-2013", "%d-%m-%Y")
+input_date_range = pd.date_range(start=start_date, end=start_date + timedelta(days=input_num_of_days) , freq="30min")[:-1]# remove HH entry form unwanted day
+
+# create index values
+group_index = [48 * [idx] for idx in range(input_sequence_len)]
+variable_index = [[idx for idx in range(48)] for iidx in range(input_sequence_len)]
+
+# flatten lists if lists
+group_index = sum(group_index, [])
+variable_index = sum(variable_index, [])
+
+# create data ranges
+group = [48 * [date_time] for date_time in input_date_range]
+variable =  [pd.date_range(start=target_data, end=target_data + timedelta(days=1) , freq="30min").tolist()[:-1] for idx in range(input_sequence_len)] # remove HH entry for next day
+
+# flatten timestamps into single list
+group = sum(group, [])
+variable = sum(variable, [])
+
+# create output time idxs
+output_time_ref = [idx for idx in range(48)]
+
+# create input time idxs
+input_time_ref = [idx for idx in range(input_sequence_len)]
+
+# input times
+input_time = [date_time for date_time in input_date_range]
+
+# output times
+output_time =  pd.date_range(start=target_data, end=target_data + timedelta(days=1) , freq="30min").tolist()[:-1]
+
+
+
+# final params for df 
+final_params = {'group_index': group_index, 
+				'variable_index': variable_index, 
+				'group': group, 
+				'variable': variable, 
+				'value': [], 
+				'input_time_ref': input_time_ref, 
+				'input_time': input_time, 
+				'input_solar': [], 
+				'input_cloud_cover': [], 
+				'output_time_ref': output_time_ref, 
+				'output_time': output_time, 
+				'prediction': []}
+
+# conver to pandas
+df = pd.DataFrame(final_params)
+df.to_clipboard()
+
+
+
+
+
